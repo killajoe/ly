46bf796755594df87e71240785bdfa2466cd51bd
Johannes Kamprad <archlinux@kamprad.net>
2025-05-27 22:27:28 +0200

    Update Matrix.zig

--- src/animations/Matrix.zig
+++ src/animations/Matrix.zig
@@ -161,7 +161,7 @@ fn draw(self: *Matrix) void {
 
 const x: usize = 0;
 var y: usize = 0;
-while (y < self.terminal_buffer.height) : (y += 1) {
+while (y < self.terminal_buffer.height) : (y +%= 1) {
     const dot = self.dots[buf_width * y + x];
     const cell = if (dot.value == null or dot.value == ' ') self.default_cell else blk: {
         const fg_color = if (dot.is_head)
